42275f1e17e325d802b87fa42945a9ae9c287042
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-02 16:14:41 +0900

    add pattern usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { css, cva } from '../styled-system/css'
+import { Stack } from '../styled-system/jsx'
 
 function App() {
   const button = cva({
@@ -22,38 +23,41 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1 className={css({ fontSize: '6xl', fontWeight: 'bold' })}>
-          Panda CSS Playground
-        </h1>
+        <Stack gap="8">
+          <h1 className={css({ fontSize: '6xl', fontWeight: 'bold' })}>
+            Panda CSS Playground
+          </h1>
 
-        <p>
-          <a href="/">Sample Link</a>
-        </p>
-        <p>
-          <a href="/" className="btn">
-            Button
-          </a>
-        </p>
+          <p>
+            <a href="/">Sample Link</a>
+          </p>
 
-        <p className={css({ bg: 'mint.500' })}>Background color is mint.</p>
+          <p>
+            <a href="/" className="btn">
+              Button
+            </a>
+          </p>
 
-        <h2
-          className={css({
-            bg: { base: 'red.500', _hover: 'red.700' },
-            fontSize: '2xl',
-          })}
-        >
-          Pseudo Style
-        </h2>
+          <p className={css({ bg: 'mint.500' })}>Background color is mint.</p>
 
-        <p>
-          <button
-            type="button"
-            className={button({ visual: 'solid', size: 'lg' })}
+          <h2
+            className={css({
+              bg: { base: 'red.500', _hover: 'red.700' },
+              fontSize: '2xl',
+            })}
           >
-            Button
-          </button>
-        </p>
+            Pseudo Style
+          </h2>
+
+          <p>
+            <button
+              type="button"
+              className={button({ visual: 'solid', size: 'lg' })}
+            >
+              Button
+            </button>
+          </p>
+        </Stack>
       </div>
     </>
   )
